90183b82d925e6af715dd4cdd282bc8b67887f7a
Abraham Sarache <75433744+abrahamsarache@users.noreply.github.com>
2021-07-21 19:42:37 +0200

    Before getting first feedback
    Before submitting to Slack team

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 const gallery = document.getElementById('gallery');
+const body = document.querySelector('body');
 
-
-const users = fetch('https://randomuser.me/api?results=12')  //
+const data = fetch('https://randomuser.me/api?results=12')
     .then(res => res.json())
     .then(data => data.results)
-    .then(users => addMembers(users))
+    .then((users) => {return users})
+    .catch((error) => {
+        console.error('Error:', error)});
+
+console.log(data);
+
+const loadUsers = data.then(user => addMembers(user));
 
 function addMembers(users){
-console.log(users);
+
  const html = users.map (user => `
     <div class="card">
 
@@ -21,18 +27,79 @@ console.log(users);
             <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
         </div>
 
-    </div>`)
-
-   // console.log(user.name.first);
+    </div>`).join('');
 
     return gallery.insertAdjacentHTML('beforeend', html);
- 
 }
 
-/*
-use the response data to display 12 users, along with some basic information for each:
-Image
-First and Last Name
-Email
-City or location*/
+    
+gallery.addEventListener('click', (event)=>{
+const divCards = gallery.querySelectorAll('div .card');
+
+for (let i=0; i<divCards.length; i++){
+    const card = divCards[i];
+
+    if(event.target === card){
+       
+        //Retrieve data for Modal window
+        const modalWindow = () =>{ data.then(user => showModal(user))};
+        
+        //Create HTML for Modal window
+        function showModal(users){
+            const birthdayData = users.map(user => user.dob.date)[i];
+            const birthdayRegex = /^(\d{4})-(\d+)-(\d+)(\w\d+\W\d+\W\d+\W\d+\w)$/;
+            const birthday = birthdayData.replace(birthdayRegex, '$2/$3/$1');
+
+            const cellData = users.map(user => user.cell)[i];
+            const cellRegex = /[^0-9]/g;
+            const removeNoNumbers = cellData.replace(cellRegex, '');
+            const onlyNumbersRegex = /(\d{3})(\d{3})(\d+)/;//(XXX) XXX-XXXX
+            const newCell = removeNoNumbers.replace(onlyNumbersRegex, '($1) $2-$3');
+            
+            const html = users.map (user =>
+                `<div class="modal-container">
+                    <div class="modal">
+                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                            <p class="modal-text">${user.email}</p>
+                            <p class="modal-text cap">${user.location.city}</p>
+                            <hr>
+                            <p class="modal-text">${newCell}</p>
+                            <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state}, ${user.location.postcode}</p>
+                            <p class="modal-text">Birthday: ${birthday}</p>
+                    </div>
+                </div>`)[i];
+
+            return body.insertAdjacentHTML('beforeend', html);
+        }
+
+        async function generateModal(){
+        await modalWindow();
+        const closeButton = await document.getElementById('modal-close-btn');
+        const modalContainer = await document.querySelector('.modal-container');
+        console.log(modalContainer);
+        
+            closeButton.addEventListener('click', (event) => {
+                modalContainer.remove();
+
+            });
+        }
+
+        generateModal();
+
+    }
+}
+})
+
+
+
+/* 
+// IMPORTANT: Below is only for exceeds tasks 
+<div class="modal-btn-container">
+<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+<button type="button" id="modal-next" class="modal-next btn">Next</button>
+</div>
+</div> */
 
