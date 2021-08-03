6eee6c66b4f5f86932f7aa6f1d3da71846d140c4
Abraham Sarache <75433744+abrahamsarache@users.noreply.github.com>
2021-08-03 22:45:40 +0200

    Submit to Slack
    Almost ready for slack community feedback

---
+++ js/ReadMe.md
@@ -0,0 +1,10 @@
+Thanks for taking the time to review my project, I highly appreciate it.
+
+I am aiming to the Meets Expectations grade.
+
+CSS changes:
+body: background color
+.card-text: color
+.modal-text: background color
+.modal: color and background color
+.card: hover
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const gallery = document.getElementById('gallery');
 const body = document.querySelector('body');
 
+//Retrieves data from API
 const data = fetch('https://randomuser.me/api?results=12')
     .then(res => res.json())
     .then(data => data.results)
@@ -8,10 +9,9 @@ const data = fetch('https://randomuser.me/api?results=12')
     .catch((error) => {
         console.error('Error:', error)});
 
-console.log(data);
-
 const loadUsers = data.then(user => addMembers(user));
 
+//Creates the members
 function addMembers(users){
 
  const html = users.map (user => `
@@ -32,74 +32,85 @@ function addMembers(users){
     return gallery.insertAdjacentHTML('beforeend', html);
 }
 
-    
-gallery.addEventListener('click', (event)=>{
-const divCards = gallery.querySelectorAll('div .card');
-
-for (let i=0; i<divCards.length; i++){
-    const card = divCards[i];
-
-    if(event.target === card){
-       
-        //Retrieve data for Modal window
-        const modalWindow = () =>{ data.then(user => showModal(user))};
-        
-        //Create HTML for Modal window
-        function showModal(users){
-            const birthdayData = users.map(user => user.dob.date)[i];
-            const birthdayRegex = /^(\d{4})-(\d+)-(\d+)(\w\d+\W\d+\W\d+\W\d+\w)$/;
-            const birthday = birthdayData.replace(birthdayRegex, '$2/$3/$1');
-
-            const cellData = users.map(user => user.cell)[i];
-            const cellRegex = /[^0-9]/g;
-            const removeNoNumbers = cellData.replace(cellRegex, '');
-            const onlyNumbersRegex = /(\d{3})(\d{3})(\d+)/;//(XXX) XXX-XXXX
-            const newCell = removeNoNumbers.replace(onlyNumbersRegex, '($1) $2-$3');
-            
-            const html = users.map (user =>
-                `<div class="modal-container">
-                    <div class="modal">
-                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                        <div class="modal-info-container">
-                            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
-                            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-                            <p class="modal-text">${user.email}</p>
-                            <p class="modal-text cap">${user.location.city}</p>
-                            <hr>
-                            <p class="modal-text">${newCell}</p>
-                            <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state}, ${user.location.postcode}</p>
-                            <p class="modal-text">Birthday: ${birthday}</p>
-                    </div>
-                </div>`)[i];
-
-            return body.insertAdjacentHTML('beforeend', html);
-        }
-
-        async function generateModal(){
-        await modalWindow();
-        const closeButton = await document.getElementById('modal-close-btn');
-        const modalContainer = await document.querySelector('.modal-container');
-        console.log(modalContainer);
-        
-            closeButton.addEventListener('click', (event) => {
-                modalContainer.remove();
-
-            });
-        }
-
-        generateModal();
-
-    }
+//Helper function to format the modal window
+function formatBirthday(birthdayData){
+     //Regex for birthday
+     const birthdayRegex = /^(\d{4})-(\d+)-(\d+)(\w\d+\W\d+\W\d+\W\d+\w)$/;
+     const birthday = birthdayData.replace(birthdayRegex, '$2/$3/$1');
+     return birthday;
 }
+
+function formatCell(cellData){
+    //Regex for cell phone
+    const cellRegex = /[^0-9]/g;
+    const removeNoNumbers = cellData.replace(cellRegex, '');
+    const onlyNumbersRegex = /(\d{3})(\d{3})(\d+)/;//(XXX) XXX-XXXX
+    const newCell = removeNoNumbers.replace(onlyNumbersRegex, '($1) $2-$3');
+    return newCell;
+}
+
+//Event listener to detect any clicks on gallery
+gallery.addEventListener('click', (event)=>{
+        
+    const divCards = document.querySelectorAll('.card')
+
+        for (let i=0; i<divCards.length; i++){
+            const card = divCards[i];
+            const cardData = card.querySelectorAll('div, img, p, h3');
+            
+            if(event.target !== gallery){
+                
+                //Retrieve data for Modal window
+                const modalWindow = () =>{ data.then(user => showModal(user))};
+                
+                //Create HTML for Modal window
+                function showModal(users){
+                    const birthdayData = users.map(user => user.dob.date)[i];
+                    const birthday = formatBirthday(birthdayData);
+
+                    const cellData = users.map(user => user.cell)[i];
+                    const newCell = formatCell(cellData);
+                    
+                    const html = users.map (user =>
+                        `<div class="modal-container">
+                            <div class="modal">
+                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                <div class="modal-info-container">
+                                    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                                    <p class="modal-text">${user.email}</p>
+                                    <p class="modal-text cap">${user.location.city}</p>
+                                    <hr>
+                                    <p class="modal-text">${newCell}</p>
+                                    <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state}, ${user.location.postcode}</p>
+                                    <p class="modal-text">Birthday: ${birthday}</p>
+                            </div>
+                        </div>`)[i];
+
+                    return body.insertAdjacentHTML('beforeend', html);
+                }
+              
+                
+                for(let i=0; i<cardData.length; i++){
+
+                    if(event.target === cardData[i]){
+                        
+                    //Creates the modal window
+                    async function generateModal(){
+                        await modalWindow(); 
+                        
+                        const closeButton = await document.getElementById('modal-close-btn');
+                        const modalContainer = await document.querySelector('.modal-container');
+                    
+                        //Remove modal window
+                        closeButton.addEventListener('click', (e) => {
+                            modalContainer.remove();
+                        });   
+                    }
+
+                    generateModal(); 
+                    }
+                }
+        }
+    }
 })
-
-
-
-/* 
-// IMPORTANT: Below is only for exceeds tasks 
-<div class="modal-btn-container">
-<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-<button type="button" id="modal-next" class="modal-next btn">Next</button>
-</div>
-</div> */
-
